fb5a3c834261cba6ad4d38173509bd86b65d557a
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-09-19 00:07:50 +0300

    3. Ders

--- PortfolioProjectNight/Controllers/AdminController.cs
+++ PortfolioProjectNight/Controllers/AdminController.cs
@@ -8,10 +8,30 @@ namespace PortfolioProjectNight.Controllers
 {
     public class AdminController : Controller
     {
-        
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
         }
     }
 }
