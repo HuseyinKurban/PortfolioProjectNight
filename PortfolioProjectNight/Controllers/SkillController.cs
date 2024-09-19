1dc0b08d775341cab9d3bad2c55fedb51ae36ed0
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-09-19 21:45:04 +0300

    .

---
+++ PortfolioProjectNight/Controllers/SkillController.cs
@@ -0,0 +1,50 @@
+﻿using PortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class SkillController : Controller
+    {
+       DbMyPortfolioNightEntities context=new DbMyPortfolioNightEntities();
+
+        public ActionResult SkillList()
+        {
+            var values=context.Skills.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateSkill(Skills skill)
+        {
+            context.Skills.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = context.Skills.Find(id);
+            context.Skills.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = context.Skills.Find(id);
+            return View(value);
+        }
+
+    }
+}
